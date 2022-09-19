ce7d27ae4379dacd2715c780d1943b76778f98ba
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-09-18 23:07:25 -0500

    Started work on settings Data component

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ const windowHeight = Dimensions.get('window').height;
 import BottomBar from './components/BottomBar';
 import TaskList from './components/TaskList.js';
 import DataManager from './components/DataManager';
+import AddTaskScreen from './components/AddTaskScreen';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,48 +15,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
   },
-
-  addTaskScreen: {
-    width: "75%",
-    height: "50%",
-    top: "25%",
-    left: "12.5%",
-    backgroundColor: "#eee",
-    position: "absolute",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  taskInput: {
-    marginLeft: "10%",
-    marginRight: "10%",
-    fontSize: "20%",
-    flexShrink: 1,
-    flexWrap: 'wrap',
-  },
-  taskAddButton: {
-    fontSize: "20%",
-    flex: 1
-  }
 });
-
-function AddTaskScreen({task, setTask, setAddTaskOpen, addTask}) {
-  return <View style={styles.addTaskScreen}>
-    <TextInput
-      style={styles.taskInput}
-      placeholder='TaskDescription'
-      value={task}
-      onChangeText={setTask}
-      multiline={true}
-    />
-
-    <Button title="Add" style={styles.taskAddButton} onPress={() => {
-      if (task == "") { return }
-      addTask(task);
-      setAddTaskOpen(false)
-    }} />
-  </View>
-}
 
 export default function App() {
   const [data, setData] = useState([])
---
+++ components/SettingsData.js
@@ -0,0 +1,26 @@
+import { getJsonData, storeData } from '../modules/storage.js';
+import { useEffect, useState } from 'react';
+
+const settingsKey = "setting:0.0"
+
+const defaultSettings = {
+    theme: "light",
+}
+
+function SettingsData({ settings, setSettings }) {
+    const [gotSettings, setGotSettings] = useState(false)
+    useEffect(() => {
+        getJsonData(settingsKey).then((d) => {
+            setGotSettings(true)
+            setSettings(d || [])
+        })
+    }, [])
+
+    useEffect(() => {
+        if (!gotSettings) { return }
+        // console.log("Stored: ", data)
+        storeData(dataKey, JSON.stringify(data)).then(() => { })
+    }, [data])
+}
+
+export { defaultSettings, SettingsData }
