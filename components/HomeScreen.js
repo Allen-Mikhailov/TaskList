f98986d89d118e503c1ae774d3bffcf4ac320824
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2023-02-03 08:09:46 -0600

    Added a settings button and worked on the home lists

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -3,7 +3,40 @@ import { StyleSheet, Text, View, Button , TextInput, ScrollView, Image } from 'r
 import { useEffect, useState } from 'react';
 import { store } from '../store.js';
 
+// Images
+const dotImg = require("../images/dot.png")
+const settingsImg = require("../images/wrench.png")
 
+function ListItem({ listId, list, navigation })
+{
+    return <View style={styles.listFrame}>
+        <Image style={styles.listDot} source={dotImg}></Image>
+        <Button title='' onPress={navigation.navigate("List:"+listId)}>
+            <Text>{list.name || "Error: NoName :("}</Text>
+        </Button>
+    </View>
+}
+
+function SettingsButton({ navigation })
+{
+    return <Button style={styles.settingsButton}>
+        <Image source={settingsImg} style={styles.fullImage}></Image>
+    </Button>
+}
+
+function HomeScreen({ route, navigation })
+{
+
+    const [loadedLists, _, updateLoadedLists ] = store.useState("loadedLists")
+    return <View style={styles.body}>
+        <Text style={styles.title}>Task List</Text>
+        <ScrollView style={styles.scroll}>
+            {Object.entries(loadedLists).map(([listId, list]) => <ListItem 
+                list={list} navigation={navigation} listId={listId}/>)}
+        </ScrollView>
+        <SettingsButton navigation={navigation}/>
+    </View>
+}
 
 const styles = StyleSheet.create({
     body: {
@@ -27,38 +60,35 @@ const styles = StyleSheet.create({
     listFrame: {
         width: "100%",
         height: "300%",
-        backgroundColor: "red"
+        backgroundColor: "red",
+        display: "flex",
+        flexDirection: "column"
     },
 
     listDot: {
-
+        aspectRatio: 1,
+        height: "100%"
     },
 
     listTitle: {
+        fontSize: "50%"
+    },
 
+    fullImage: {
+        width: "100%",
+        height: "100%",
+    },
+
+    settingsButton: {
+        position: "absolute",
+        width: "10%",
+        aspectRatio: 1,
+        right: "10px",
+        bottom: "10px"
     }
+
+
 })
 
-function ListItem({list})
-{
-    return <View style={styles.listFrame}>
-        <Image style={styles.listDot}></Image>
-        <Text>{list.name || "Test"}</Text>
-    </View>
-}
-
-function HomeScreen()
-{
-
-    const [loadedLists, _, updateLoadedLists ] = store.useState("loadedLists")
-    return <View style={styles.body}>
-        <Text style={styles.title}>Task List</Text>
-        <ScrollView style={styles.scroll}>
-            {Object.entries(loadedLists).map(([listId, list]) => <ListItem list={list}>
-
-            </ListItem>)}
-        </ScrollView>
-    </View>
-}
 
 export default HomeScreen;
